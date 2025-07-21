4244b477caba33900af82c7c2ee79c78cacd4529
Sergei Silnov <po@kumekay.com>
2025-07-21 16:25:38 +0200

    fix: Add SQT to num layer (#1)
    * fix: Add SQT to num layer
    * Cleanup for aronia

--- miryoku/mapping/42/crabapplepad.h
+++ miryoku/mapping/42/crabapplepad.h
@@ -30,3 +30,16 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 #endif
 
 #define MIRYOKU_LAYER_BASE MIRYOKU_LAYER_BASE_CRABAPPLEPAD
+
+
+#if !defined(MIRYOKU_LAYER_NUM_CRABAPPLEPAD)
+
+#define MIRYOKU_LAYER_NUM_CRABAPPLEPAD \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SQT,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#endif
+
+#define MIRYOKU_LAYER_NUM MIRYOKU_LAYER_NUM_CRABAPPLEPAD
